bc28e3c3e2ddda1417fde3b66f521f8188a7d803
hungvietmai <hungmvhe163655@fpt.edu.vn>
2023-07-17 20:39:47 +0700

    Add login module

---
+++ app/src/main/java/com/example/homeactivity/Views/RegisterActivity.java
@@ -0,0 +1,53 @@
+package com.example.homeactivity.Views;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.homeactivity.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextUsername;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+
+                if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                } else {
+
+
+                    registerUser(username, email, password);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String username, String email, String password) {
+
+        Toast.makeText(this, "Đăng ký thành công!", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}

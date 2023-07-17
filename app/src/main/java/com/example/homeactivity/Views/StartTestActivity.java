0461200ef8bf92bdd50a070f7c6df3b239e5b939
daoduc220901 <90316727+concuacon1@users.noreply.github.com>
2023-07-18 04:23:47 +0700

    score activity

--- app/src/main/java/com/example/homeactivity/Views/StartTestActivity.java
+++ app/src/main/java/com/example/homeactivity/Views/StartTestActivity.java
@@ -6,18 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.example.homeactivity.Controllers.StudySetController;
+import com.example.homeactivity.Controllers.TermController;
 import com.example.homeactivity.R;
 
 public class StartTestActivity extends AppCompatActivity {
     private TextView tvNumberOfQuestionStt;
-    StudySetController studySetController;
+    TermController termController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_test);
+
+        termController = new TermController();
+
         ((Button)findViewById(R.id.btn_start)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,10 +32,17 @@ public class StartTestActivity extends AppCompatActivity {
         tvNumberOfQuestionStt = findViewById(R.id.tv_numberOfQuestionStt);
 
         Intent intent = getIntent();
-        String studySetId = intent.getStringExtra("studySetId");
-        studySetController.listAllStudySets(studySetId, studySet -> {
-            tvNumberOfQuestionStt.setText(studySet.size());
+        //String studySetId = intent.getStringExtra("studySetId");
+        termController.listAllTerms("rdKlzh42W3RR3r8T59hp", studySet -> {
+            tvNumberOfQuestionStt.setText(String.valueOf(studySet.size()));
         });
 
+        ((ImageButton)findViewById(R.id.btn_back)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartTestActivity.this,StudySetActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
